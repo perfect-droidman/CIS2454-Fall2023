bb77208bc8869d15646023a5b6799c29a6dca44f
Gabriele Grosse <56041468+perfect-droidman@users.noreply.github.com>
2023-09-09 11:19:12 -0400

    Created new folder and file for lab0

---
+++ Lab0/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About Partner</title>
+    </head>
+    <body>
+      <?php
+        echo "<h2>Tahreem Bhatti</h2>";
+       
+        echo "1. Tahreem Bhatti I am doing computer software engineering I am taking this class because it's required for my degree.";
+        echo "2. I took all the cis class for this degree and it's my last semester in occ so all the class that counts for the degree.";
+        echo "3. I love to go hiking and dancing too.";
+        echo "4. I do have plans other than studies like my brother's destination wedding";
+        echo "5. She did not have anything else to say.";
+      ?>
+    </body>
+</html>
